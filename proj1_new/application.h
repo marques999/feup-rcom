a23f1dfb2797ceef70028406020d5680d81e3521
marques999 <xmarques999@hotmail.com>
2015-10-17 12:41:17 +0100

    deleted old code

--- proj1_new/application.h
+++
@@ -1,9 +0,0 @@
-#ifndef __APPLICATION_H_
-#define __APPLICATION_H_
-
-typedef struct {
-	int fd; /*Descritor correspondente à porta série*/
-	int status; /*TRANSMITTER | RECEIVER*/
-} ApplicationLayer;
-
-#endif /* __APPLICATION_H_ */
