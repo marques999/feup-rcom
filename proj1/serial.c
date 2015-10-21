b0179b54fafb9c8c306aaa4220ef59d39b51de08
Diogo Marques <xmarques999@hotmail.com>
2015-10-22 00:48:32 +0100

    code formatting

--- proj1/serial.c
+++ proj1/serial.c
@@ -3,26 +3,21 @@
 
 #define _POSIX_SOURCE 1
 
-int checkPath(char* ttyPath) {
-	return strcmp("/dev/ttyS0", ttyPath) == 0 ||
-			strcmp("/dev/ttyS1", ttyPath) == 0 ||
-			strcmp("/dev/ttyS4", ttyPath) == 0 ||
-			strcmp("/dev/ttyS5", ttyPath) == 0;
-}
-
-static void clearBuffer() {	
+static void clearBuffer()
+{
 	int c; while ((c = getchar()) != '\n' && c != EOF);
 }
 
-static int readInteger(int start, int end) {
-
+static int readInteger(int start, int end)
+{
 	int input;
 
-	while (1) {
-		
+	while (1)
+	{
 		printf("? ");
 
-		if (scanf("%d", &input) == 1 && input >= start && input <= end) {
+		if (scanf("%d", &input) == 1 && input >= start && input <= end)
+		{
 			break;
 		}
 
@@ -33,15 +28,16 @@ static int readInteger(int start, int end) {
 	return input;
 }
 
-static char* readString() {
-	
-	char* input =  malloc(MAX_SIZE * sizeof(char));
+static char* readString()
+{
+	char* input = malloc(MAX_SIZE * sizeof(char));
 
-	while (1) {
-
+	while (1)
+	{
 		printf("? ");
 
-		if (scanf("%s", input) == 1) {
+		if (scanf("%s", input) == 1)
+		{
 			break;
 		}
 
@@ -50,42 +46,28 @@ static char* readString() {
 	}
 
 	return input;
-}
-
-size_t getFileSize(FILE* file) {
-
-	size_t currentPosition = ftell(file);
-	size_t currentSize;
-
-	if (fseek(file, 0, SEEK_END) == -1) {
-		return -1;
-	}
-
-	currentSize = ftell(file);
-	fseek(file, 0, currentPosition);
-
-	return currentSize;
 }
 
 #define PROGRESS_LENGTH		40
 
-void writeProgress(double current, double total) {
-
+void writeProgress(double current, double total)
+{
 	const double percentage = 100.0 * current / total;
-	const int pos = (int) (percentage * PROGRESS_LENGTH / 100.0);
+	const int pos = (int)(percentage * PROGRESS_LENGTH / 100.0);
 
 	printf("\rCompleted: %6.2f%% [", percentage);
 
-	int i;	
-	for (i = 0; i < PROGRESS_LENGTH; i++) {
+	int i;
+	for (i = 0; i < PROGRESS_LENGTH; i++)
+	{
 		i <= pos ? printf("=") : printf(" ");
 	}
 
 	puts("]\n");
 }
 
-int main(int argc, char** argv) {
-	
+int main(int argc, char** argv)
+{
 	puts("---------------------------------");
 	puts("|\tRCOM FILE TRANSFER\t|");
 	puts("---------------------------------");
@@ -95,7 +77,8 @@ int main(int argc, char** argv) {
 	int connectionMode = readInteger(1, 2) - 1;
 	int connectionBaudrate = -1;
 
-	while (connectionBaudrate == -1) {
+	while (connectionBaudrate == -1)
+	{
 		puts("\n> ENTER TRANSMISSION BAUD RATE:");
 		puts("\t[200, 300, 600, 1200, 1800, 2400, 4800, 9600, 19200, 38400, 57600]\n");
 		connectionBaudrate = link_getBaudrate(readInteger(200, 57600));
@@ -113,34 +96,41 @@ int main(int argc, char** argv) {
 	char portName[20] = "/dev/ttySx";
 	portName[9] = '0' + numPort;
 
-	if (connectionMode == MODE_TRANSMITTER) {
+	if (connectionMode == MODE_TRANSMITTER)
+	{
 		puts("\n> ENTER SOURCE FILENAME:");
-	} else {
+	}
+	else
+	{
 		puts("\n> ENTER DESTINATION FILENAME:");
 	}
 
 	char* fileName = readString();
-	int fd = link_initialize(portName, connectionMode, connectionBaudrate, 
-			connectionRetries, connectionTimeout);
-	
+	int fd = link_initialize(portName, connectionMode, connectionBaudrate,
+							 connectionRetries, connectionTimeout);
+
 	logConnection();
-		
-	if (fd < 0) {
+
+	if (fd < 0)
+	{
 		return -1;
 	}
-	
+
 	application_init(portName, fd, connectionMode, fileName);
-	
+
 	unsigned char out[255];
 	unsigned char data[] = { 'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!' };
 
-    if (connectionMode == MODE_TRANSMITTER) {
-		llwrite(fd, data, sizeof(data)/sizeof(data[0]));
-	} else {
+	if (connectionMode == MODE_TRANSMITTER)
+	{
+		llwrite(fd, data, sizeof(data) / sizeof(data[0]));
+	}
+	else
+	{
 		llread(fd, out);
 	}
 
 	llclose(fd, connectionMode);
 
-    return 0;
+	return 0;
 }
